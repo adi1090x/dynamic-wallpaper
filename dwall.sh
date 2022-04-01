fb450ce5647e2f1354391ee6d073fad2d8f5261d
Stefan Garlonta <70283087+garlontas@users.noreply.github.com>
2022-04-01 14:28:03 +0200

    Add support for budgie desktop environment

--- dwall.sh
+++ dwall.sh
@@ -127,7 +127,7 @@ case "$OSTYPE" in
 				SETTER=set_cinnamon
 			elif [[ "$DESKTOP_SESSION" =~ ^(/usr/share/xsessions/plasma|NEON|Neon|neon|PLASMA|Plasma|plasma|KDE|Kde|kde)$ ]]; then
 				SETTER=set_kde
-			elif [[ "$DESKTOP_SESSION" =~ ^(PANTHEON|Pantheon|pantheon|GNOME|Gnome|gnome|Gnome-xorg|gnome-xorg|UBUNTU|Ubuntu|ubuntu|DEEPIN|Deepin|deepin|POP|Pop|pop)$ ]]; then
+			elif [[ "$DESKTOP_SESSION" =~ ^(PANTHEON|Pantheon|pantheon|GNOME|Gnome|gnome|Gnome-xorg|gnome-xorg|UBUNTU|Ubuntu|ubuntu|DEEPIN|Deepin|deepin|POP|Pop|pop|budgie-desktop)$ ]]; then
 				SETTER="gsettings set org.gnome.desktop.background picture-uri"
 			else 
 				SETTER="feh --bg-fill"
